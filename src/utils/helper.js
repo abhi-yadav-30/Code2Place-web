c4ec1432dc2a96fe297024cbf84800a8604a348d
Abhinandan Yadav <cs21b1011@iiitdm.ac.in>
2025-11-22 20:02:25 +0530

    hisk

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,5 +1,6 @@
 export const getDomain = () => {
-  if (import.meta.env.VITE_MODE == "dev") {
+    const mode="prod"
+  if (mode == "dev") {
     return "http://localhost:5000";
   } else {
     return "https://code2place-api2.onrender.com";
