8d605c5ee2fa0807c13ffa98892c0ffe4b4f861f
Abhinandan Yadav <cs21b1011@iiitdm.ac.in>
2025-11-22 22:30:14 +0530

    ghvhgv

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 
